7df41db8c8b1e10733d99e555336321f4db1a4a7
Brad <b3r1a3dtw@gmail.com>
2022-05-18 01:32:30 +0800

    c

--- Son.java
+++ Son.java
@@ -1,1 +1,6 @@
-public class Son extends Father{}
+public class Son extends Father{
+    public void habit(){  //也會有habit這支method
+        System.out.println("I love racing."); //把fishing隱藏
+        super.habit();
+    }
+}
