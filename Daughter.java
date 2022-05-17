2d85e6f17e3a351e1cf1004111321ca5610c8983
Brad <b3r1a3dtw@gmail.com>
2022-05-18 01:46:32 +0800

    d

---
+++ Daughter.java
@@ -0,0 +1,7 @@
+public class Daughter extends Son{
+    public void habit(){
+        System.out.println("I love playing dolls.");
+        super.habit();
+    }
+    
+}
--- Inheritance.java
+++ Inheritance.java
@@ -2,5 +2,7 @@ public class Inheritance{
     public static void main(String [] args){
         Son s=new Son();
         s.habit();
+        Daughter d=new Daughter();
+        d.habit();
     }
 }
