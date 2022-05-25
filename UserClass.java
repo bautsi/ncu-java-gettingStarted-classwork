96e19c1d91c6799b2ed6c3dc68a815d85fe1cb32
bautsi <b3r1a3dtw@gmail.com>
2022-05-25 12:40:57 +0800

    a

--- UserClass.java
+++ UserClass.java
@@ -1,9 +1,9 @@
-public class UserClass{
-    public static void main(String[] args){
-        //Animal a=new Animal();
-        Animal a=new Dog();
-        a.move();
-        a=new Bird();
-        a.move();
+public class UserClass {
+    public static void main(String [] args){
+        C c=new A();
+        //c=new　B(); //B是抽象的 不能把它變成物件
+        c.ma(); //A沒有ma 但可以去找阿公C
+        c.mb();
+        //c.mc();
     }
 }
